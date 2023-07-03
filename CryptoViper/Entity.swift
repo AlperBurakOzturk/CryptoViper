3e52c82a2a1921a871c8d4cedf7c432f250d8d5a
Alper Burak Öztürk <alperrburak@gmail.com>
2023-07-04 00:15:06 +0300

    Initial Commit

---
+++ CryptoViper/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Alper Burak Öztürk on 20.04.2023.
+//
+
+import Foundation
+
+// Struct
+
+struct Crypto: Decodable {
+    let currency: String
+    let price: String
+}
